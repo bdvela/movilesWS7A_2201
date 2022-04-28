7350f043528fa89050b862b9ef3a85810ad14e42
bdvela <bdvela20@gmail.com>
2022-04-28 08:52:04 -0500

    py joke app

---
+++ App_Sem0502_Agenda_20220422/app/src/main/java/com/example/app_sem0502_agenda_20220422/prototype_contact.kt
@@ -0,0 +1,11 @@
+package com.example.app_sem0502_agenda_20220422
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class prototype_contact : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.prototype_contact)
+    }
+}
---
+++ App_Sem061_Networking/app/src/main/java/com/example/app_sem06_1_networking/Joke.kt
@@ -0,0 +1,8 @@
+package com.example.app_sem06_1_networking
+
+import com.google.gson.annotations.SerializedName
+
+class Joke (
+    @SerializedName("joke")
+    val joke: String
+)
